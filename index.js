c1a789f45d59c0c3d8af082d7a8576fba72a2ff3
almant12 <dokoalmant123@gmail.com>
2024-12-01 21:20:01 +0100

    update the name

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const EmailValidator = require('./src/emailValidator');
 const PasswordValidator = require('./src/PasswordValidator')
 const FileValidator = require('./src/FileValidator')
 
-const almantZod = {
+/**
+ * v is a lightweight validation library that provides various input validation methods.
+ */
+const v = {
   file: () => new FileValidator(),
   email: (options) => new EmailValidator(options),
   password: () => new PasswordValidator(),
@@ -14,4 +17,4 @@ const almantZod = {
   object: (schema) => new ObjectSchema(schema)
 };
 
-module.exports = almantZod;
+module.exports = v;
